f6c6d54e38d3faf6ab27b2f79efde3d5b89726ef
Timothy Dao <timster78811@gmail.com>
2018-08-15 15:05:40 -0400

    finished routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+
 const { PORT, MONGODB_URI } = require('./config');
 
 const notesRouter = require('./routes/notes');
@@ -46,6 +47,19 @@ app.use((err, req, res, next) => {
 
 // Listen for incoming connections
 if (process.env.NODE_ENV !== 'test') {
+  // Connect to DB and Listen for incoming connections
+  mongoose
+    .connect(MONGODB_URI)
+    .then(instance => {
+      const conn = instance.connections[0];
+      console.info(
+        `Connected to: mongodb://${conn.host}:${conn.port}/${conn.name}`
+      );
+    })
+    .catch(err => {
+      console.error(err);
+    });
+
   app
     .listen(PORT, function() {
       console.info(`Server listening on ${this.address().port}`);
@@ -54,30 +68,5 @@ if (process.env.NODE_ENV !== 'test') {
       console.error(err);
     });
 }
-// Connect to DB and Listen for incoming connections
-mongoose
-  .connect(
-    MONGODB_URI,
-    { useNewUrlParser: true }
-  )
-  .then(instance => {
-    const conn = instance.connections[0];
-    console.info(
-      `Connected to: mongodb://${conn.host}:${conn.port}/${conn.name}`
-    );
-  })
-  .catch(err => {
-    console.error(`ERROR: ${err.message}`);
-    console.error('\n === Did you remember to start `mongod`? === \n');
-    console.error(err);
-  });
-
-app
-  .listen(PORT, function() {
-    console.info(`Server listening on ${this.address().port}`);
-  })
-  .on('error', err => {
-    console.error(err);
-  });
 
 module.exports = app; // Export for testing
